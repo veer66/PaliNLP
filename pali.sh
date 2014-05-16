9c25416712ccfe8fe4daf4223ee9f7655cd2ca9d
Vee Satayamas <vsatayamas@gmail.com>
2014-05-16 07:12:12 +0700

    add shell script for calling console convieniently

---
+++ pali.sh
@@ -0,0 +1,2 @@
+#!/bin/sh
+java -cp bin:data/extlib/jackson-core-2.2.3.jar:data/extlib/jackson-databind-2.2.3.jar:data/extlib/jackson-annotations-2.2.3.jar:data/extlib/2014-01-19_LibDictionaryClientRecompiled.jar de.unitrier.daalft.pali.PaliConsole
